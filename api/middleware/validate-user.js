55af183f35a812b1fefedb0258652417fab3d904
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-10-20 14:03:57 -0500

    Created migration for category table

---
+++ api/middleware/validate-user.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets');
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token){
+    // check the token is valid
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      
+      if(err){
+        // foul play
+        res.status(401).json({message: "YOU SHALL NOT PASS!"})
+      } else {
+        // Token is gooooooooooood
+        next();
+      }
+
+    })
+  } else {
+    res.status(400).json({ message: 'No credentials provided' });
+  }
+};
---
+++ database/dev/migrations/20191020134709_catagories_table.js
@@ -0,0 +1,11 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('category', tbl => {
+    tbl.increments()
+    tbl.string('categoryName', 255)
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('category')
+};
